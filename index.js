ddbdf4d34d1bcec0a450339818b47508ab431c4b
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-06 19:40:36 -0300

    Post Tweets Done

--- index.js
+++ index.js
@@ -15,4 +15,10 @@ app.post("/sign-up", (req, res) => {
     res.send("OK");
 });
 
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    tweets.push({ username, tweet });
+    res.send("OK");
+});
+
 app.listen(5000, () => console.log("listening on port 5000"));
