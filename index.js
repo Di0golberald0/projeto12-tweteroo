180d61ef7edb6223ad77853f61637b2f00eec50b
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-06 19:35:07 -0300

    Post Sign-Up Done

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    users.push({ username, avatar });
+    res.send("OK");
+});
+
+app.listen(5000, () => console.log("listening on port 5000"));
